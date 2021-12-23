db5decc4b7df72eb836c6eb69994dedee456657a
thh <hzjmessage@outlook.com>
2021-12-23 11:19:15 +0800

    update all

---
+++ d.sh
@@ -0,0 +1,10 @@
+hugo
+cd public
+git init 
+git add .
+git commit -m 'build'
+git remote add origin git@github.com:xknowledge/xknowledge.github.io.git
+git push -f -u origin master
+cd ..
+# git@github.com:xknowledge/xknowledgebuild.git
+
